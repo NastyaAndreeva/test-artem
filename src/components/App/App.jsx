2a41eff89e75e52150a2c7923e3c6f4a6bfde817
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-07-07 14:13:08 +0300

    show only profile component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
 import { Profile } from 'components/ProfileTask/Profile/Profile';
 
-import { Statistics } from 'components/Statistics/Statistics';
-import dataStatistics from '../../data/data';
+// import { Statistics } from 'components/Statistics/Statistics';
+// import dataStatistics from '../../data/data';
 
-import { FriendList } from 'components/FriendTask/FriendList/FriendList';
-import dataFriends from '../../data/friends.json';
+// import { FriendList } from 'components/FriendTask/FriendList/FriendList';
+// import dataFriends from '../../data/friends.json';
 
-import { TransactionHistory } from 'components/Transaction/TransactionHistory/TransactionHistory';
-import dataTransations from '../../data/transactions.json';
+// import { TransactionHistory } from 'components/Transaction/TransactionHistory/TransactionHistory';
+// import dataTransations from '../../data/transactions.json';
 
 export const App = () => {
   return (
