0b3fcf9c44cd11a79f6aaa4931fbaa8a32a7b43e
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-07-07 14:46:42 +0300

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Profile } from 'components/ProfileTask/Profile/Profile';
 
 import { Statistics } from 'components/Statistics/Statistics';
-import dataStatistics from '/data/data';
+import dataStatistics from './data/data';
 
 import { FriendList } from 'components/FriendTask/FriendList/FriendList';
-import dataFriends from '/data/friends';
+import dataFriends from './data/friends';
 
 import { TransactionHistory } from 'components/Transaction/TransactionHistory/TransactionHistory';
-import dataTransations from '/data/transactions';
+import dataTransations from './data/transactions';
 
 export const App = () => {
   return (
