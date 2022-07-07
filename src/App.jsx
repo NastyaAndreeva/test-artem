ce944bb3f40d45c3fe10f2635ede058379ae1f55
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-07-07 14:37:33 +0300

    fixed json settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import { Profile } from 'components/ProfileTask/Profile/Profile';
 
 console.log('Profile: ', Profile);
 
-// import { Statistics } from 'components/Statistics/Statistics';
-// import dataStatistics from '../../data/data';
+import { Statistics } from 'components/Statistics/Statistics';
+import dataStatistics from '../../data/data';
 
-// import { FriendList } from 'components/FriendTask/FriendList/FriendList';
-// import dataFriends from '../../data/friends.json';
+import { FriendList } from 'components/FriendTask/FriendList/FriendList';
+import dataFriends from '../../data/friends.json';
 
-// import { TransactionHistory } from 'components/Transaction/TransactionHistory/TransactionHistory';
-// import dataTransations from '../../data/transactions.json';
+import { TransactionHistory } from 'components/Transaction/TransactionHistory/TransactionHistory';
+import dataTransations from '../../data/transactions.json';
 
 export const App = () => {
   return (
     <>
       <Profile />
-      {/* <Statistics title="Upload stats" stats={dataStatistics} />
+      <Statistics title="Upload stats" stats={dataStatistics} />
       <FriendList friends={dataFriends} />
-      <TransactionHistory transactions={dataTransations} /> */}
+      <TransactionHistory transactions={dataTransations} />
     </>
   );
 };
